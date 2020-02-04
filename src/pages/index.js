106cd2e67d264a31a825b362c10cf58b0a76b578
Firman Jamal <haruthehero@gmail.com>
2020-02-04 20:28:16 +0800

    Typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
             <h1>Introduction</h1>
             {/* <p>My name is Firman Jamal, I live in Sembawang, Singapore and currently working on <a href="https://www.kibblepaw.com" target="__blank" rel="noopener noreferrer">KibblePaw</a> - a platform for people to adopt or sell merchandise associating with Home Pets.</p> */}
             <p>My name is Firman Jamal and my hobby is programming, developing software and designing. I am a freelance full-stack developer on Fiverr. I am passionate about developing new things, solving new problems and love to give advice to others for their projects.</p>
-            <p>I am currently working on my own Projects called <a href="https://www.kibblepaw.com" target="__blank" rel="noopener noreferrer">KibblePaw</a> - a platform for people to adopt or sell merchandise associating with Pets.</p>
+            <p>I am currently working on my own Project called <a href="https://www.kibblepaw.com" target="__blank" rel="noopener noreferrer">KibblePaw</a> - a platform for people to adopt or sell merchandise associating with Pets.</p>
             <p>I graduated from <a href="https://www.rp.edu.sg" target='_blank' rel="noopener noreferrer">Republic Polytechnic</a> in Woodlands, Singapore with <a href="https://www.rp.edu.sg/SOI/full-time-diplomas/Details/diploma-in-information-technology" target='_blank' rel="noopener noreferrer">Diploma in Infomation Technology</a> on 2020.</p>
             <h3>The skills that I have acquired in school</h3>
             <ol>
