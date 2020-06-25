81542736e313b1276c3ab9edc91b7e1da620537d
MUHAMAD FIRMANAIN B JAMALUDDIN <17044634@myrp.edu.sg>
2020-06-26 05:25:02 +0800

    Added code block support

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
 import Title from '../components/title';
 import Layout from '../components/layout';
+import _ from 'lodash';
 
 export const query = graphql`
     query($slug: String!) {
@@ -39,6 +39,13 @@ const Blog = (props) =>  {
             {
                 documentToReactComponents(props.data.contentfulBlogPost.body.json, {
                     renderNode: {
+                        "paragraph": (node, children) => {
+                            if (node.content.length === 1 && _.find(node.content[0].marks, {type: 'code'})) {
+                                return <pre><code>{node.content[0].value}</code></pre>;
+                            }
+
+                            return <p>{children}</p>
+                        },
                         "embedded-asset-block": (node) => {
                             return <img src={node.data.target.fields.file['en-US'].url} alt={node.data.target.fields.title['en-US']}  />
                         }
